6cab9bf02d3886edee5d143bccd3ede81a5ccff4
renovat503 <85083316+renovat503@users.noreply.github.com>
2022-05-18 21:14:40 -0600

    test

--- src/components/ShopNow.jsx
+++ src/components/ShopNow.jsx
@@ -22,7 +22,39 @@ const ShopNow = () => {
                         </Box>
                     </div>
                     <div className="col">
-                    <Box p="2" mt="2" mb="2" borderRadius="7" w="100%" h="250px" bg="gray.200">
+                        <Box p="2" mt="2" mb="2" borderRadius="7" w="100%" h="250px" bg="gray.200">
+                                <Image src={blackair} borderRadius="7"/>
+                                <Box mt="2"><Text fontWeight="500" fontSize="14">Off-White x Nike AF1 Low "MoMa" </Text></Box>
+                                <Box mt="0.5" display="flex">
+                                    <Text fontWeight="500" color="gray.500" fontSize="14">$119.99</Text>
+                                    <Badge fontSize="9" colorScheme="green" ml="2"><Text mt="1">in stock</Text></Badge>
+                                </Box>
+                                <Box display="flex" justifyContent="space-between">
+                                    <Text fontSize="10" fontStyle="italic" color="gray.400">by Renova</Text>
+                                    <Box display="flex"><TiStarFullOutline color="black"/><Text ml="1" fontSize="12">4.8</Text></Box>
+                                </Box>
+                            </Box>
+                    </div>
+                    <div className="col"> 
+                        <Box p="2" mt="2" mb="2" borderRadius="7" w="100%" h="250px" bg="gray.200">
+                                <Image src={blackair} borderRadius="7"/>
+                                <Box mt="2"><Text fontWeight="500" fontSize="14">Off-White x Nike AF1 Low "MoMa" </Text></Box>
+                                <Box mt="0.5" display="flex">
+                                    <Text fontWeight="500" color="gray.500" fontSize="14">$119.99</Text>
+                                    <Badge fontSize="9" colorScheme="green" ml="2"><Text mt="1">in stock</Text></Badge>
+                                </Box>
+                                <Box display="flex" justifyContent="space-between">
+                                    <Text fontSize="10" fontStyle="italic" color="gray.400">by Renova</Text>
+                                    <Box display="flex"><TiStarFullOutline color="black"/><Text ml="1" fontSize="12">4.8</Text></Box>
+                                </Box>
+                            </Box>  
+                    </div>
+
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <Box p="2" mt="2" mb="2" borderRadius="7" w="100%" h="250px" bg="gray.200">
                             <Image src={blackair} borderRadius="7"/>
                             <Box mt="2"><Text fontWeight="500" fontSize="14">Off-White x Nike AF1 Low "MoMa" </Text></Box>
                             <Box mt="0.5" display="flex">
@@ -35,19 +67,33 @@ const ShopNow = () => {
                             </Box>
                         </Box>
                     </div>
+                    <div className="col">
+                        <Box p="2" mt="2" mb="2" borderRadius="7" w="100%" h="250px" bg="gray.200">
+                                <Image src={blackair} borderRadius="7"/>
+                                <Box mt="2"><Text fontWeight="500" fontSize="14">Off-White x Nike AF1 Low "MoMa" </Text></Box>
+                                <Box mt="0.5" display="flex">
+                                    <Text fontWeight="500" color="gray.500" fontSize="14">$119.99</Text>
+                                    <Badge fontSize="9" colorScheme="green" ml="2"><Text mt="1">in stock</Text></Badge>
+                                </Box>
+                                <Box display="flex" justifyContent="space-between">
+                                    <Text fontSize="10" fontStyle="italic" color="gray.400">by Renova</Text>
+                                    <Box display="flex"><TiStarFullOutline color="black"/><Text ml="1" fontSize="12">4.8</Text></Box>
+                                </Box>
+                            </Box>
+                    </div>
                     <div className="col"> 
-                    <Box p="2" mt="2" mb="2" borderRadius="7" w="100%" h="250px" bg="gray.200">
-                            <Image src={blackair} borderRadius="7"/>
-                            <Box mt="2"><Text fontWeight="500" fontSize="14">Off-White x Nike AF1 Low "MoMa" </Text></Box>
-                            <Box mt="0.5" display="flex">
-                                <Text fontWeight="500" color="gray.500" fontSize="14">$119.99</Text>
-                                <Badge fontSize="9" colorScheme="green" ml="2"><Text mt="1">in stock</Text></Badge>
-                            </Box>
-                            <Box display="flex" justifyContent="space-between">
-                                <Text fontSize="10" fontStyle="italic" color="gray.400">by Renova</Text>
-                                <Box display="flex"><TiStarFullOutline color="black"/><Text ml="1" fontSize="12">4.8</Text></Box>
-                            </Box>
-                        </Box>  
+                        <Box p="2" mt="2" mb="2" borderRadius="7" w="100%" h="250px" bg="gray.200">
+                                <Image src={blackair} borderRadius="7"/>
+                                <Box mt="2"><Text fontWeight="500" fontSize="14">Off-White x Nike AF1 Low "MoMa" </Text></Box>
+                                <Box mt="0.5" display="flex">
+                                    <Text fontWeight="500" color="gray.500" fontSize="14">$119.99</Text>
+                                    <Badge fontSize="9" colorScheme="green" ml="2"><Text mt="1">in stock</Text></Badge>
+                                </Box>
+                                <Box display="flex" justifyContent="space-between">
+                                    <Text fontSize="10" fontStyle="italic" color="gray.400">by Renova</Text>
+                                    <Box display="flex"><TiStarFullOutline color="black"/><Text ml="1" fontSize="12">4.8</Text></Box>
+                                </Box>
+                            </Box>  
                     </div>
 
                 </div>
