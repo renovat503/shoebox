b6440cca26ecae2f9b138e6ca00bb64fcb858315
Renovat Mbonimpa <Renovat.Mbonimpa@biofiredx.com>
2022-05-23 00:19:26 -0600

    det

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -7,6 +7,13 @@ import {
     ModalBody,
     ModalCloseButton,useDisclosure
   } from '@chakra-ui/react';
+  import {
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+  } from '@chakra-ui/react'
 import { Box,Container,Button ,Image,Text,Badge} from "@chakra-ui/react";
 import { useToast } from '@chakra-ui/react'
 import blackair from "../assets/blackair.jpeg";
@@ -46,27 +53,100 @@ const Product = () => {
                         </Box>
                     </div>
 
-                    <Modal size="4xl" isOpen={isOpen} onClose={onClose} isCentered motionPreset='slideInBottom'>
+                    <Modal size="6xl" isOpen={isOpen} onClose={onClose} isCentered motionPreset='slideInBottom'>
                         <ModalOverlay />
                         <ModalContent>
                         <ModalHeader>Prodcut Details</ModalHeader>
                         <ModalCloseButton bg="black" color="white"/>
                         <ModalBody>
+                            <div className='row'>
                         <div className="col margintwo" >
-                        <Box p="2" mt="2" mb="4" borderRadius="7" minW="312px" h="95%" bg="white" position="relative">
-                            <Box zIndex="2" right="3" top="4" position="absolute"></Box>
-                            <Image src={blackair} borderRadius="7"/>
-                            <Box mt="2"><Text fontWeight="500" fontSize="14">Off-White x Nike AF1 Low "MoMa" </Text></Box>
-                            <Box mt="0.5" display="flex">
-                                <Text fontWeight="500" color="gray.500" fontSize="14">$119.99</Text>
-                                <Badge fontSize="9" colorScheme="green" ml="2"><Text mt="1">in stock</Text></Badge>
+                            <Box p="2" mt="2" mb="4" borderRadius="7" minW="312px" h="95%" bg="white" position="relative">
+                                <Box zIndex="2" right="3" top="4" position="absolute"></Box>
+                                <Image src={blackair} borderRadius="7"/>
                             </Box>
-                            <Box display="flex" justifyContent="space-between">
-                                <Text fontSize="10" fontStyle="italic" color="gray.400">by Renova</Text>
-                                <Box display="flex"><TiStarFullOutline color="black"/><Text ml="1" fontSize="12">4.8</Text></Box>
+                        </div>
+                        <div className="col margintwo" >
+                            <Box p="2" mt="2" mb="4" borderRadius="7" minW="312px" h="95%" bg="white" position="relative">
+                                <Box mt="2"><Text fontWeight="500" fontSize="14">Off-White x Nike AF1 Low "MoMa" </Text></Box>
+                                    <Box mt="0.5" display="flex">
+                                        <Text fontWeight="500" color="gray.500" fontSize="14">$119.99</Text>
+                                        <Badge fontSize="9" colorScheme="green" ml="2"><Text mt="1">in stock</Text></Badge>
+                                    </Box>
+                                <Box display="flex" justifyContent="space-between">
+                                    <Text fontSize="10" fontStyle="italic" color="gray.400">by Renova</Text>
+                                    <Box display="flex"><TiStarFullOutline color="black"/><Text ml="1" fontSize="12">4.8</Text></Box>
+                                </Box>
+                                <Accordion>
+                                    <AccordionItem>
+                                        <h2>
+                                        <AccordionButton>
+                                            <Box flex='1' textAlign='left'>
+                                            Size
+                                            </Box>
+                                            <AccordionIcon />
+                                        </AccordionButton>
+                                        </h2>
+                                        <AccordionPanel pb={4}>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                        commodo consequat.
+                                        </AccordionPanel>
+                                    </AccordionItem>
+
+                                    <AccordionItem>
+                                        <h2>
+                                        <AccordionButton>
+                                            <Box flex='1' textAlign='left'>
+                                            Color
+                                            </Box>
+                                            <AccordionIcon />
+                                        </AccordionButton>
+                                        </h2>
+                                        <AccordionPanel pb={4}>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                        commodo consequat.
+                                        </AccordionPanel>
+                                    </AccordionItem>
+                                    <AccordionItem>
+                                        <h2>
+                                        <AccordionButton>
+                                            <Box flex='1' textAlign='left'>
+                                            Description
+                                            </Box>
+                                            <AccordionIcon />
+                                        </AccordionButton>
+                                        </h2>
+                                        <AccordionPanel pb={4}>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                        commodo consequat.
+                                        </AccordionPanel>
+                                    </AccordionItem>
+                                    <AccordionItem>
+                                        <h2>
+                                        <AccordionButton>
+                                            <Box flex='1' textAlign='left'>
+                                            Delivery & return
+                                            </Box>
+                                            <AccordionIcon />
+                                        </AccordionButton>
+                                        </h2>
+                                        <AccordionPanel pb={4}>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                        commodo consequat.
+                                        </AccordionPanel>
+                                    </AccordionItem>
+                                    </Accordion>
                             </Box>
-                        </Box>
-                    </div>
+                        </div>
+                        </div>
                         </ModalBody>
 
                         <ModalFooter>
